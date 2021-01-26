883c52e8684b78fd64e6b24dd738911e20e40ed4
Jvdjvdjvd <Jarek.vd@gmail.com>
2021-01-26 20:18:14 +0100

    finished most of ex. 4. added empty files to add folders to repo

--- assignment1/ex4.py
+++ assignment1/ex4.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 #Set global paramameters
 pm = 0.2
@@ -9,12 +10,43 @@ def bitflipper(original_vector, flip_chance):
     idxes = [i for i in range(len(original_vector)) if random.random() < flip_chance]
     subtracter = np.zeros(len(original_vector)).astype(int)
     subtracter[idxes] = 1
-    xm = np.absolute(x - subtracter)
+    xm = np.absolute(original_vector - subtracter)
     return xm
 
+def get_fitness(x): #TODO: check if this is indeed the fitness function..
 
-#start ex4
-x = np.random.randint(2, size=genLen)
-xm = bitflipper(x, pm)
+    return sum(x)
 
-#TODO: implement the rest of question 4
+def do_genetic_run(length, iterations, pm, alwaysReplace = False):
+    x = np.random.randint(2, size=length)
+    xList = [[] for _ in range(iterations+1)]
+    fitnessList = [0 for _ in range(iterations+1)]
+    fitnessList[0] = get_fitness(x)
+    xList[0] = x
+
+    for i in range(1,iterations+1):
+        xList[i] = x
+        xm = bitflipper(x, pm)
+        if get_fitness(xm) > get_fitness(x) or alwaysReplace:
+            x = xm
+        xList[i] = x
+        fitnessList[i] = get_fitness(x)
+    return xList, fitnessList
+
+if __name__ == '__main__':
+    # question 4a
+    Xs,Fitnesses = do_genetic_run(100,1500,1/100)
+    plt.plot(list(range(0,1501)), Fitnesses)
+
+    #4b
+    plt.figure()
+    for i in range(10):
+        Xs, Fitnesses = do_genetic_run(100, 1500, 1 / 100)
+        plt.plot(list(range(0, 1501)), Fitnesses)
+
+    #4c
+    Xs,Fitnesses = do_genetic_run(100,1500,1/100, alwaysReplace=True)
+    plt.plot(list(range(0,1501)), Fitnesses)
+
+
+#TODO: check if the questions are actually answered
